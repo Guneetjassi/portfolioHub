469175b7dcff82e4f50c5b2e4f0929ea27a2ee1e
happyapplesauce <141449101+happyapplesauce@users.noreply.github.com>
2024-08-10 17:48:30 -0600

    Updated contact page

--- app/contact/page.js
+++ app/contact/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Contact() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-6 bg-black-100">
-      {/* Navigation Bar */}
+    
       <nav className="bg-gray-800 text-white w-full p-4 fixed top-0 left-0">
         <div className="container mx-auto flex justify-between items-center">
           <div className="text-2xl font-bold">
@@ -19,7 +19,7 @@ export default function Contact() {
         </div>
       </nav>
 
-      {/* Contact Section */}
+      
       <section className="bg-white py-20 mt-16 shadow-md rounded-lg w-full max-w-3xl mx-auto">
         <div className="text-center">
           <h2 className="text-4xl font-bold text-gray-800 mb-8">Get in Touch</h2>
@@ -38,14 +38,7 @@ export default function Contact() {
             <button 
             type="submit" 
             className="w-full bg-blue-500 text-white px-6 py-3 rounded hover:bg-blue-600 transition-all">Send Message</button>
-          </form>
-          <div className="mt-12 text-center">
-            <p className="text-lg text-gray-800">Connect with me social sites:</p>
-            <div className="space-x-4 mt-4">
-              <a href="https://linkedin.com" className="text-blue-700 hover:text-blue-800 text-2xl">LinkedIn</a>
-              <a href="https://github.com" className="text-gray-800 hover:text-black text-2xl">GitHub</a>
-            </div>
-          </div>
+          </form>  
         </div>
       </section>
     </main>
